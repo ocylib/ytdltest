7c09b64aaddb6b82987e586c179e617270f9f764
OCY <zichengou@gmail.com>
2021-12-12 17:21:43 +0800

    ss

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const Innertube = require('youtubei.js');
 const http = require('http');
 (async () => {
 	const server = http.createServer(async (req, res) => {
-		const youtube = await new Innertube();
-		const stream = youtube.download('oxFr7we3LC8', {
-			type: 'videoandaudio',
-		});
-		res.writeHead(200, {
-			'Content-Type': `video/mp4`,
-		});
-		stream.pipe(res);
+		try {
+			const youtube = await new Innertube();
+			const stream = youtube.download('oxFr7we3LC8', {
+				type: 'videoandaudio',
+			});
+			res.writeHead(200, {
+				'Content-Type': `video/mp4`,
+			});
+			stream.pipe(res);
+		} catch (error) {
+			console.log('ERROR:', error);
+		}
 	});
 	server.listen(3000, () => console.log('running'));
 })();
