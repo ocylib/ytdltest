bead410cc522a2be809ef6414098f256fa1bf4ff
OCY <zichengou@gmail.com>
2021-12-12 15:48:39 +0800

    cookie

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const rangeParser = require('range-parser');
 const http = require('http');
 const PORT = process.env.PORT || 3000;
 
+const COOKIE =
+	'HSID=A3TZvCxY8Jj5K1cN4; SSID=APHEIK0oT1PupOP3x; APISID=tX076O1X_ADIcKUG/ATQRenoQhuPAeIGrg; SAPISID=3yPEacNbaw3CfVTH/AYpxJrjJ_lYXgRIf6; __Secure-1PAPISID=3yPEacNbaw3CfVTH/AYpxJrjJ_lYXgRIf6; __Secure-3PAPISID=3yPEacNbaw3CfVTH/AYpxJrjJ_lYXgRIf6; VISITOR_INFO1_LIVE=UYdfOWWH8J8; PREF=tz=Asia.Shanghai; _ga=GA1.2.1975201126.1635152974; NID=511=uLfoAMI6lm7-dljyAMhtsfJ4Z8UScsg-TD3dNZGwOXDRvbsf6wgFO30-sGOLKkyFQR4T14LCAJ6M-i9RBGnQqCkGSrPTBU87AAlgV7YItow__mNC8JAjbM0b0t4f0jnc-Ctv9Uua3lh6eqX2Whzikk1c3YszY5IIMzdHl_NssbA; SID=Egjrbj3ahPONgQZFS3WgryfRUWy4byDil-rp8KqQH-g0K_fVcwYlXIqToG08phmpRPaZeQ.; __Secure-1PSID=Egjrbj3ahPONgQZFS3WgryfRUWy4byDil-rp8KqQH-g0K_fVtqa-dmm9IoSq-AoMHQzGgA.; LOGIN_INFO=AFmmF2swRQIgL5J2Tf4O_SLdGHSt9AsKXiGK6mZwZ2zJfQCe7xNLFPICIQDFKb-lcYT2pRlJjwAHRWk4I6k5lFyUPeVtFZOUa1s-nA:QUQ3MjNmeGxxTU9rODg5V1hLZmRBazlwbGZIcld1a0FCSG5iZ09BbTMwdUthSG1MUjBRcmhqSG8zR1JyVUJsR1hWX253RGdPcnluT2hnY3oyZGsxQnloSTU0aVdHc3JWWUtoSm5JbkFqQkhQTGFreWNPZF9nRm1HOWhmTnVkYlZPYkh3MkpGQ2xDVTZ0RnNrUFh4YWtycWpueE1VbTFrWldn; __Secure-3PSID=Egjrbj3ahPONgQZFS3WgryfRUWy4byDil-rp8KqQH-g0K_fVuJ8IHzoNPOBR4i5PdFKmDw.; YSC=Y4gunyIPN1U';
+
 const server = http.createServer(async (req, res) => {
 	try {
 		const url = 'https://www.youtube.com/watch?v=oxFr7we3LC8';
@@ -12,6 +15,11 @@ const server = http.createServer(async (req, res) => {
 		const options = {
 			filter: 'audioandvideo',
 			quality: 'highestvideo',
+			requestOptions: {
+				headers: {
+					cookie: COOKIE,
+				},
+			},
 		};
 		const info = await getInfo(url);
 		let { contentLength, container } = chooseFormat(info.formats, options);
